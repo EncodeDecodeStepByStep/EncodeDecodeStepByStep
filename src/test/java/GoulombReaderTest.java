d70f07d23652b8f6c1f3cb98cde23716a23d1bec
Bruno Pagliarini Pozzebon <bruno.pozzebon@cwi.com.br>
2020-08-19 23:40:00 -0300

    melhorando

--- src/test/java/GoulombReaderTest.java
+++ src/test/java/GoulombReaderTest.java
@@ -9,13 +9,13 @@ public class GoulombReaderTest {
     @Test
     public void deveFazerOEncodeDoArquivo() throws IOException {
         Goulomb goulomb = new Goulomb(32);
-        goulomb.encode(new File("src/main/resources/filesToEncode/arquivoFacil.txt"));
+        goulomb.encode(new File("src/main/resources/filesToEncode/alice29.txt"));
     }
 
     @Test
     public void deveFazerDecodeDoArquivo() throws IOException {
         Goulomb goulomb = new Goulomb(32);
-        goulomb.decode(new File("src/main/resources/filesEncoded/arquivoFacil.txt.cod"));
+        goulomb.decode(new File("src/main/resources/filesEncoded/alice29.txt.cod"));
     }
 
 }
