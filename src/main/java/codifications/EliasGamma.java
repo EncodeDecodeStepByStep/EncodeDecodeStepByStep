e1a465f17b90c43e35e814dfe62a1cebf9c51e17
Bruno Camboim <bruno.camboim1997@gmail.com>
2020-09-05 17:22:56 -0300

    decode - elais gamma

--- src/main/java/codifications/EliasGamma.java
+++ src/main/java/codifications/EliasGamma.java
@@ -37,32 +37,33 @@ public class EliasGamma implements Codification {
         Reader reader = new Reader(file);
         Writer writer = new Writer(DECODED_FOLDER+file.getName());
 
-//        boolean alreadyFoundStopBit = false;
-//
-//        int digitsOnRest = MathUtils.logBase2(this.divisor);
-//        int quocient =  0;
-//        char character;
-//
-//        while((character= (char) reader.read())!=65535){
-//            if(!alreadyFoundStopBit){
-//                if((character-'0') == STOP_BIT){
-//                    alreadyFoundStopBit = true;
-//                }else{
-//                    quocient++;
-//                }
-//            }else{
-//                String restInBinary = "";
-//                restInBinary+=character;
-//                for (int i =1; i<digitsOnRest; i++){
-//                    restInBinary+=reader.read()-'0';
-//                }
-//                int rest = Integer.parseInt(restInBinary,2);
-//                char finalNumber = (char) ((quocient * this.divisor) + rest);
-//                writer.write(finalNumber);
-//                quocient = 0;
-//                alreadyFoundStopBit = false;
-//            }
-//        }
+        boolean alreadyFoundStopBit = false;
+        int prefixLength = 0;
+        char character;
+
+        while ((character = (char)reader.read()) != 65535) {
+        	if (!alreadyFoundStopBit && (character-'0') == STOP_BIT) {
+        		alreadyFoundStopBit = true;
+        	} else {
+        		if (!alreadyFoundStopBit) {
+        			prefixLength++;
+        			continue;
+        		}
+
+        		String restInBinary = "";
+                restInBinary += character;
+                for (int i = 1; i < prefixLength; i++) {
+                    restInBinary += reader.read()-'0';
+                }
+                
+                int rest = Integer.parseInt(restInBinary,2);
+                char finalNumber = (char) ((int) Math.pow(2, prefixLength) + rest);
+                writer.write(finalNumber);
+                
+                alreadyFoundStopBit = false;
+                prefixLength = 0;
+        	}
+        }
         writer.close();
         reader.close();
     }
