5001d6bc128c36251a91b4edf950a8a453939dc0
Gustavo Steinmetz <stzgustavo@gmail.com>
2020-11-16 19:37:00 -0300

    update path in Reader Writer Wrapper

--- backend/src/main/java/br/unisinos/encodedecodestepbystep/domain/ReaderWriterWrapper.java
+++ backend/src/main/java/br/unisinos/encodedecodestepbystep/domain/ReaderWriterWrapper.java
@@ -27,6 +27,7 @@ public class ReaderWriterWrapper {
     }
 
     public static ReaderWriterWrapper getDecodeReaderWriterWrapperRedundancy(String path, MutableDouble progressPercentage) throws IOException {
+        path = path.replace("\\", "/");
         return new ReaderWriterWrapper(new ReaderRedundancy(new File(path), progressPercentage),
                 new WriterRedundancy(path.substring(0, path.lastIndexOf("/") + 1) + "decoded_" + path.substring(path.lastIndexOf("/") + 1).replaceFirst("(?s)(.*)" + "[.][^.]+$", "$1" + "")));
     }
@@ -36,6 +37,7 @@ public class ReaderWriterWrapper {
     }
 
     public static ReaderWriterWrapper getDecodeReaderWriterWrapperNormal(String path, MutableDouble progressPercentage) throws IOException {
+        path = path.replace("\\", "/");
         return new ReaderWriterWrapper(new Reader(new File(path), progressPercentage),
                 new Writer(path.substring(0, path.lastIndexOf("/") + 1) + "decoded_" + path.substring(path.lastIndexOf("/") + 1).replaceFirst("(?s)(.*)" + "[.][^.]+$", "$1" + "")));
     }
