44bc44bfe0f3a63951df22c977b243016a320960
Gustavo Steinmetz <stzgustavo@gmail.com>
2020-11-16 14:14:41 -0300

    update SystemUtils para path codeword repository padrao para tds sistemas operacionais

--- backend/src/main/java/br/unisinos/encodedecodestepbystep/utils/SystemUtils.java
+++ backend/src/main/java/br/unisinos/encodedecodestepbystep/utils/SystemUtils.java
@@ -5,10 +5,6 @@ public class SystemUtils {
     public static final String OS = System.getProperty("os.name").toLowerCase();
 
     public static String getCodewordsRepositoryPath(){
-        if(OS.contains("nix") ||OS.contains("nux") ||OS.contains("aix")) {
-            return System.getProperty("user.dir") + "/linux-unpacked/public/backend_jar/database/CodewordsSizesArray.repository";
-        } else {
-            return System.getProperty("user.dir") + "/public/backend_jar/database/CodewordsSizesArray.repository";
-        }
+        return System.getProperty("user.dir") + "/public/backend_jar/database/CodewordsSizesArray.repository";
     }
 }
